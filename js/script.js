996a5e570f7e28d8b918a5581e288f14e1a482cb
RafaelApfernandes97 <109108455+RafaelApfernandes97@users.noreply.github.com>
2023-03-28 14:39:31 -0300

    28031439

--- js/script.js
+++ js/script.js
@@ -31,6 +31,23 @@ window.onload = function () {
 
 
 
+//opacidade da imagem quando selecionada
+
+function toggleCheckbox(img) {
+    var checkbox = img.nextElementSibling;
+    if (checkbox.checked) {
+        checkbox.checked = false;
+        img.style = "opacity: 10";
+    } else {
+        checkbox.checked = true;
+        img.style = "opacity: 0.5";
+    }
+}
+
+
+
+
+
 
 
 
